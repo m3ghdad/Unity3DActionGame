18b5325a3252aaec5891637bc8271c79d5a65116
m3ghdad <meghdadabbaszadegan@Meghdads-MacBook-Pro.local>
2017-05-15 19:08:45 -0700

    first commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance = null;
+
+	[SerializeField] GameObject player;
+	[SerializeField] GameObject[] spawnPoints;
+	[SerializeField] GameObject tanker;
+	[SerializeField] GameObject ranger;
+	[SerializeField] GameObject soldier;
+	[SerializeField] Text levelText;
+	private bool gameOver = false;
+	private int currentLevel;
+	private float generatedSpawnTime = 1;
+	private float currentSpawnTime = 0;
+	private GameObject newEnemy;
+
+	private List <EnemyHealth> enemies = new List<EnemyHealth> ();
+	private List <EnemyHealth> killedEnemies = new List<EnemyHealth> ();
+
+	public void RegisterEnemy(EnemyHealth enemy) {
+		enemies.Add (enemy);
+	}
+
+	public void KilledEnemies (EnemyHealth enemy) {
+		killedEnemies.Add (enemy);
+	}
+
+	public bool GameOver {
+		get {return gameOver; }
+	}
+		public GameObject Player {
+		get {return player;}
+	}
+
+	void Awake()
+	{
+		if (instance == null) {
+			instance = this;
+
+		} else if (instance != this){
+			Destroy (gameObject);
+		}
+		DontDestroyOnLoad (gameObject);
+	}
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (spawn ());
+		currentLevel = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		currentSpawnTime += Time.deltaTime;
+	}
+
+	public void PlayerHit(int currentHP) {
+
+		if (currentHP > 0) {
+			gameOver = false;
+		} else {
+			gameOver = true;
+		}
+	}
+
+	IEnumerator spawn() {
+
+		if (currentSpawnTime > generatedSpawnTime) {
+			currentSpawnTime = 0;
+
+			if (enemies.Count < currentLevel) {
+
+				int randomNumber = Random.Range (0, spawnPoints.Length - 1);
+				GameObject spawnLocation = spawnPoints [randomNumber];
+				int randomEnemy = Random.Range (0, 3);
+				if (randomEnemy == 0) {
+					newEnemy = Instantiate (soldier) as GameObject;
+				} else if (randomEnemy == 1) {
+					newEnemy = Instantiate (ranger) as GameObject;
+				} else if (randomEnemy == 2) {
+					newEnemy = Instantiate (tanker) as GameObject;
+				}
+
+				newEnemy.transform.position = spawnLocation.transform.position;
+					
+			}
+
+			if (killedEnemies.Count == currentLevel) {
+
+				enemies.Clear ();
+				killedEnemies.Clear ();
+
+				yield return new WaitForSeconds (3f);
+				currentLevel++;
+				levelText.text = "Level " + currentLevel;
+			}
+		}
+
+		yield return null;
+		StartCoroutine (spawn ());
+	}
+	
+
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Assertions;
+
+public class PlayerHealth : MonoBehaviour {
+
+	[SerializeField] int startingHealth = 100;
+	[SerializeField] float timeSinceLastHit =2f;
+	[SerializeField] Slider healthSlider;
+	private float timer = 0f;
+	private CharacterController charactorController;
+	private Animator anim;
+	private int currentHealth;
+	private AudioSource audio;
+	private ParticleSystem blood;
+
+	void Awake()
+	{
+		Assert.IsNotNull (healthSlider);
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+		anim = GetComponent<Animator>();
+		charactorController = GetComponent<CharacterController> ();
+		currentHealth = startingHealth;
+		audio = GetComponent<AudioSource> ();
+		blood = GetComponentInChildren<ParticleSystem> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+	}
+	
+		void OnTriggerEnter(Collider other)
+	{
+		if (timer >= timeSinceLastHit && !GameManager.instance.GameOver) {
+
+			if (other.tag == "Weapon") {
+				takeHit ();
+				timer = 0;
+			}
+		}
+	}
+
+	void takeHit () {
+		if (currentHealth > 0) {
+			GameManager.instance.PlayerHit (currentHealth);
+			anim.Play ("Hurt");
+			currentHealth -= 10;
+			healthSlider.value = currentHealth;
+			audio.PlayOneShot (audio.clip);
+			blood.Play ();
+		}
+
+		if (currentHealth <= 0) {
+			killPlayer();
+		}
+	}
+
+	void killPlayer() {
+		GameManager.instance.PlayerHit (currentHealth);
+		anim.SetTrigger("HeroDie");
+		charactorController.enabled = false;
+		audio.PlayOneShot (audio.clip);
+		blood.Play ();
+	}
+	
+}
